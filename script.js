b251bcd85733e5db41fce69e8cee0339a6e950b4
Tushar Pramanik <tushar.pramanik@lastingdynamics.com>
2024-01-07 11:35:15 +0600

    implemented game logic and reset game logic

--- script.js
+++ script.js
@@ -3,6 +3,26 @@
 const checkBtn = document.querySelector(".check");
 const guessInput = document.querySelector(".guess");
 const messageText = document.querySelector(".message");
+const scoreText = document.querySelector(".score");
+const againBtn = document.querySelector(".again");
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+const decreaseScore = () => {
+  score--;
+  scoreText.textContent = score;
+};
+
+const handleResetGame = () => {
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  score = 20;
+
+  scoreText.textContent = score;
+  messageText.textContent = "Start guessing...";
+
+  guessInput.value = "";
+};
 
 const handleNumberInput = () => {
   // If no number is typed
@@ -17,11 +37,24 @@ const handleNumberInput = () => {
   // If typed number is less than 1 or greater than 20
   if (inputValue < 1 || inputValue > 20) {
     messageText.textContent = "🔞 Invalid Number!!!";
-    return;
+  } else if (inputValue === secretNumber) {
+    messageText.textContent = "🎉 Correct Number!!!";
+  } else if (inputValue > secretNumber) {
+    if (score >= 0) {
+      messageText.textContent = "📈 Too High!!!";
+      decreaseScore();
+    } else {
+      messageText.textContent = "👎 You Lost The Game!!!";
+    }
+  } else if (inputValue < secretNumber) {
+    if (score >= 0) {
+      messageText.textContent = "📉 Too Low!!!";
+      decreaseScore();
+    } else {
+      messageText.textContent = "👎 You Lost The Game!!!";
+    }
   }
-
-  // If typed number is valid
-  console.log(inputValue);
 };
 
 checkBtn.addEventListener("click", handleNumberInput);
+againBtn.addEventListener("click", handleResetGame);
